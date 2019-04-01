f37d4590e047b9af28cb41ee5559f23d1e0260cb
lifang <12345678>
2019-04-01 17:55:17 +0800

    20190401

---
+++ src/main/java/data/Demo4.java
@@ -0,0 +1,12 @@
+package data;
+
+public class Demo4 {
+    public static void main(){
+        int[] a = {1, 2, 3, 4};
+        int[] b = {2, 4};
+        int[] c = {1, 3};
+        int[] d = {2};
+
+
+    }
+}
--- src/main/java/set/HashSetDemo1.java
+++ src/main/java/set/HashSetDemo1.java
@@ -1,7 +1,5 @@
 package set;
 
-import java.util.HashMap;
-
 public class HashSetDemo1 {
     public static void main(String[] args){
 
